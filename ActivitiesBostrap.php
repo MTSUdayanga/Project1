87f8bfd4b0ef41ef5a393e8011040289de69d8aa
MTSUdayanga <sachithgalle@gmail.com>
2018-09-06 17:27:06 +0530

    #17imageSlider

--- ActivitiesBostrap.php
+++ ActivitiesBostrap.php
@@ -6,6 +6,8 @@
         <script src="js/ProfilePic.js" type="text/javascript"></script>
         <link href="Plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
         <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        
+
     </head>
     <body style="background-color:#FFFFFF ">
         <div class="row">
@@ -31,7 +33,7 @@
         <h3> tables </h3>
         <div class="container">
             <table class="table">
-                <thead class="thead-dark">
+                <thead>
                     <tr>
                         <th>No</th>
                         <th>Name</th>
@@ -58,7 +60,133 @@
 
         <br> <br> <br> 
 
+        <h3>  Image  </h3>
+        <div class="container">
+            <img src="images/car.jpg" class="img-thumbnail" alt="Cinque Terre" width="304" height="236"> 
 
+        </div>
+
+        <br> <br> <br> 
+        <h4>  jambootorn  </h4>
+        <div class="alert alert-success alert-dismissible ">
+            <button type="button" class="close" data-dismiss="alert">&times;</button>
+            <strong>Success!</strong> This is it.
+        </div>
+        <div class="container">
+            <div class="jumbotron">
+                <img src="images/car.jpg" class="img-thumbnail" alt="Cinque Terre" width="304" height="299" style="left: 50px"> 
+            </div>
+
+            <button type="button" class="btn btn-danger"> Confirm </button>
+
+
+        </div>
+
+
+        <br> <br> <br>
+
+        <h4>Progress bar</h4>
+
+        <style>
+            #myProgress {
+                width: 50%;
+                background-color: #ddd;
+            }
+
+            #myBar {
+                width: 1%;
+                height: 20px;
+                background-color: #4CAF50;
+            }
+        </style>
+
+
+        <div id="myProgress">
+            <div id="myBar"></div>
+        </div>
+        <br>
+        <button onclick="move()">Click Me</button> 
+
+        <br><br><br>
+
+        <div class="container">
+            <h2>Pagination</h2>
+            <p>Click the button 1..2..........</p>                  
+            <ul class="pagination">
+                <li><a href="index.php">1</a></li>
+                <li><a href="ActivitiesBostrap.php">2</a></li>
+
+            </ul>
+        </div> 
+
+
+        <div class="container">
+            <div class="panel panel-default">
+                <div class="panel-heading">Panel Heading</div>
+                <div class="panel-body">Panel Content</div>
+            </div>            
+        </div>
+
+
+        <br><br><br>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <script>
+            function move() {
+                var elem = document.getElementById("myBar");
+                var width = 1;
+                var id = setInterval(frame, 520);
+                function frame() {
+                    if (width >= 100) {
+                        clearInterval(id);
+                    } else {
+                        width++;
+                        elem.style.width = width + '%';
+                    }
+                }
+            }
+        </script>
+
+
+
+
+
+        <script src="jquery.min.js"></script>
+        <script src="owlcarousel/owl.carousel.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <script src="Plugins/fontawesome/js/fontawesome.min.js" type="text/javascript"></script>
---
+++ ImageSlider.php
@@ -0,0 +1,71 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link href="Plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <link href="Plugins/OwlCarousel/dist/assets/owl.carousel.min.css" rel="stylesheet" type="text/css"/>
+        <link href="Plugins/OwlCarousel/dist/assets/owl.theme.default.min.css" rel="stylesheet" type="text/css"/>
+        <title>Slider</title>
+        <style>
+            .item {
+                height: 200px;
+                width: 200px;
+                background: #cccccc;
+                color: #fff;
+                margin: 10px;
+            }
+        </style>
+
+
+    </head>
+    <body>
+
+
+
+        <div class="owl-carousel">
+
+
+            <div class="item"><h4>1</h4><img src="images/car.jpg"></div>
+            <div class="item"><h4>2</h4>  <img src="images/33b869f90619e81763dbf1fccc896d8d.jpg"></div>
+            <div class="item"><h4>3</h4> <img src="images/maxresdefault.jpg"></div>
+            <div class="item"><h4>4</h4> <img src="images/logo-intro2.png"></div>
+            <div class="item"><h4>5</h4> <img src="images/logo-intro2.png"></div>
+            <div class="item"><h4>6</h4> <img src="images/profileconfig.png"></div>
+            <div class="item"><h4>7</h4> <img src="images/male-user-profile-picture_318-37825.jpg"></div>
+            <div class="item"><h4>8</h4> <img src="images/logo-intro.png"></div>
+            <div class="item"><h4>9</h4> <img src="images/images.jpg"></div>
+
+        </div>
+
+
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+                <!--<script src="jquery.min.js"></script>-->
+        <script src="Plugins/OwlCarousel/dist/owl.carousel.min.js" type="text/javascript"></script>
+
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="Plugins/fontawesome/js/fontawesome.min.js" type="text/javascript"></script>
+
+        <script>
+            $(document).ready(function () {
+                $('.owl-carousel').owlCarousel({
+                    loop: true,
+                    margin: 100,
+                    nav: true,
+                    responsive: {
+                        0: {
+                            items: 1
+                        },
+                        600: {
+                            items: 3
+                        },
+                        1000: {
+                            items: 5
+                        }
+                    }
+                })
+            });
+
+        </script>
+
+    </body>
+</html>
