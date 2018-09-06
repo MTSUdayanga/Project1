aaabc88fde42728708627f5aef0534de4d3e1747
MTSUdayanga <sachithgalle@gmail.com>
2018-09-06 10:44:08 +0530

    bosstrap#17.php

---
+++ ActivitiesBostrap.php
@@ -0,0 +1,69 @@
+<html>
+    <head>
+        <title>Bostrap Activities</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link href="css/ProfilePic.css" rel="stylesheet" type="text/css"/>
+        <script src="js/ProfilePic.js" type="text/javascript"></script>
+        <link href="Plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body style="background-color:#FFFFFF ">
+        <div class="row">
+            <div class="col" style="background-color: #0999f9">1 Coloum</div>
+            <div class="col" style="background-color: #e83e8c"><2nd Coloum</div>
+            <div class="col"  style="background-color: #a7cc38">3rd Coloum</div>
+
+        </div>
+        <br> <br> <br>    
+
+
+        <h3> Coloum Bostrap   </h3>
+
+        <div class="row">
+
+            <div class="col-sm-8" style="background-color: #059fc0"> This is it </div>
+            <div class="col-sm-4" style="background-color: #d39e00 "> This is... </div>
+
+        </div>
+
+        <br> <br> <br> 
+
+        <h3> tables </h3>
+        <div class="container">
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>No</th>
+                        <th>Name</th>
+                    </tr>
+
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>  s1  </td>
+                        <td>  Kamal </td>
+                    </tr>
+
+                    <tr>
+                        <td>  s2  </td>
+                        <td>  lal </td>
+                    </tr>
+
+                </tbody>
+
+            </table>
+
+        </div>
+
+        <br> <br> <br> 
+
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="Plugins/fontawesome/js/fontawesome.min.js" type="text/javascript"></script>
+
+
+
+    </body>
+</html>
